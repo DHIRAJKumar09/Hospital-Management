1bb25ffc42990041cc66b76093b6f164423bf7f2
DHIRAJKumar09 <dhirajsinghania09@gmail.com>
2024-11-25 09:22:51 +0530

    nextauth file update

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -9,7 +9,8 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-};
+});
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+// Exporting HTTP methods (GET and POST) for the Next.js App Router
+export const GET = handler;
+export const POST = handler;
